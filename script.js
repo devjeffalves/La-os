3e0a508b2c69500c80e40d2a116bb73ddfcd7e5f
Dev Jeff Alves <jeff-alves@outlook.com>
2023-05-08 21:54:40 -0300

    lacos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Exercício 1
 function somaNumeros() {
 let valor = 100;
 let soma = 0
@@ -9,5 +10,70 @@ return soma;
 }
 
 console.log(somaNumeros());
+*/
 
+// Exercício 2
+// Escreva uma função que receba um array com números e devolva qual o maior
+// dentro dele
 
+/*
+const numeros = [12,14,18,20,10,8];
+function maiorNumero(numeros){
+    let maior = 0;
+   for (let i = 0; i < numeros.length; i++){
+    if(numeros[i] > maior) {
+        maior = numeros[i]
+    }
+ }
+ return maior; 
+}
+console.log(numeros)
+console.log(maiorNumero(numeros))
+*/
+
+//Exercício 3
+
+/*const palavras = ['Oi','sumido','tudo','bem?','Saudades!'];
+function construirFrase(arrayPalavras) {
+    let frase = '';
+    for (let palavra of palavras) {
+    console.log(palavra);
+    frase = frase + ' ' + palavra;
+    }
+return frase
+    }
+console.log(construirFrase(palavras).trim())
+*/
+
+let paisesAr = ['Brasil','Argentina','Rússia','Lichten'];
+let contadorAr = 1;
+for(let pais of paisesAr) {
+    console.log(contadorAr++ + '- ' + pais + '/BR');
+    switch(pais) {
+        case 'Brasil':
+            break;
+            default:
+                break;
+    }
+}
+console.log(contadorAr++ + '- ' + pais + '/AR');
+switch(pais) {
+    case 'Argentina':
+        break;
+        default:
+            break;
+}
+console.log(contadorAr++ + '- ' + pais + 'RU');
+switch(pais) {
+    case 'Rússia':
+        break;
+        default:
+            break;
+}
+console.log(contadorAr++ + '- ' + pais + '/LI');
+switch(pais) {
+    case 'Lichtein':
+        break;
+        default:
+            break;
+}
